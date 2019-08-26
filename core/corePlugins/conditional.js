b49323f21aea84963a36e3f9d417636f1be94942
manuhank <manuhank@gmail.com>
2019-08-26 12:02:56 -0300

    git reflog

--- core/corePlugins/conditional.js
+++ core/corePlugins/conditional.js
@@ -1,23 +1,34 @@
 function conditional(request, arrayOfPreviousResponses) {
   for (var i in request) {
-    var condition = parseReferencesInString(i, arrayOfPreviousResponses);
+    var condition;
+    if (i === "else") {
+      condition = "true";
+    } else {
+      condition = parseReferencesInString(i, arrayOfPreviousResponses);
+    }
     try {
       if (eval(condition)) {
-        var services = request[i][syntax.services];
-        var requests = request[i][syntax.requests];
-        var responses = arrayOfPreviousResponses.slice();
-        responses.push([]);
-        var thisThread = responses.length - 1;
-        runThreadOfServices(
-          services,
-          requests,
-          arrayOfPreviousResponses,
-          responses[thisThread]
-        );
-        if (responses[thisThread].length) {
-          var lastResponse = responses[thisThread].length - 1;
-          return responses[thisThread][lastResponse];
-        }
+        if (
+          typeof request[i] === "object" &&
+          request[i].hasOwnProperty(syntax.services) &&
+          request[i].hasOwnProperty(syntax.requests)
+        ) {
+          var services = request[i][syntax.services];
+          var requests = request[i][syntax.requests];
+          var responses = arrayOfPreviousResponses.slice();
+          responses.push([]);
+          var thisThread = responses.length - 1;
+          runThreadOfServices(
+            services,
+            requests,
+            arrayOfPreviousResponses,
+            responses[thisThread]
+          );
+          if (responses[thisThread].length) {
+            var lastResponse = responses[thisThread].length - 1;
+            return responses[thisThread][lastResponse];
+          }
+        } else return request[i];
       }
     } catch (error) {
       return { error: error, condition: condition };
--- plugins/Airtable/airtable.js
+++ plugins/Airtable/airtable.js
@@ -1,4 +1,4 @@
-function airtable(request) {
+function airtable(request, arrayOfPreviusRequests) {
   var options = {
     headers: {
       Authorization: "Bearer " + request.token
@@ -18,18 +18,27 @@ function airtable(request) {
     options.method = "post";
     options.headers["Content-Type"] = "application/json";
     options.payload = JSON.stringify({ fields: request.post });
-  } else if (request.hasOwnProperty("patch")) {
-    options.method = "patch";
-    options.headers["Content-Type"] = "application/json";
-    url = url + "/" + encodeURI(request.patch["id"]);
-    delete request.patch["id"];
-    options.payload = JSON.stringify({ fields: request.patch });
   } else if (request.hasOwnProperty("delete")) {
     options.method = "delete";
     //GAS entiende delete como operador asi q esto:
     url = url + "/" + encodeURI(request["delete"]);
+  } else if (request.hasOwnProperty("patch")) {
+    options.method = "patch";
+    options.headers["Content-Type"] = "application/json";
+    url = url + "/" + encodeURI(request.patch["__id"]);
+    delete request.patch["__id"];
+    options.payload = JSON.stringify({ fields: request.patch });
   }
-
+  for (var i in request) {
+    var id = parseReferencesInString(i, arrayOfPreviusRequests);
+    if (id.substr(0, 3) === "rec") {
+      options.method = "patch";
+      options.headers["Content-Type"] = "application/json";
+      url = url + "/" + encodeURI(id);
+      options.payload = JSON.stringify({ fields: request[i] });
+      break
+    }
+  }
   var response = JSON.parse(UrlFetchApp.fetch(url, options));
   if (response.hasOwnProperty("error")) {
     response.requestPeticionada = request;
