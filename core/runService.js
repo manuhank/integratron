2cc322f76641e3ab90cc0d3ddad957d8a1f3b62b
manuhank <manuhank@gmail.com>
2019-09-09 00:32:28 -0300

    a

--- core/runService.js
+++ core/runService.js
@@ -3,7 +3,7 @@
 //responsesThread must be an array
 //optionally, it posible to pass an array of previous responses
 function runService(service, request, responsesThread) {
-  if (loadPlugins().hasOwnProperty(service)) {
+  if (itsAPlugin(service)) {
     return this[service](
       findAndParseReferences(request, responsesThread),
       responsesThread
--- plugins/plugins.js
+++ plugins/plugins.js
@@ -1,5 +1,5 @@
 var plugins = {
   airtable: {},
   tarjetasMercadoPago: {},
-  validateEmail : {}
+  validateEmail: {}
 };
